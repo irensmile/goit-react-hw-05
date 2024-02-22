ce1d7b49705cf7b22b433eb1f4240b09ff0140b2
irensmile <curkan.ira@gmail.com>
2024-02-22 12:53:50 -0500

    Add a message on loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,15 +1,18 @@
 import { ThreeCircles } from "react-loader-spinner";
 
 export const Loader = () => (
-  <ThreeCircles
-    visible={true}
-    height="30"
-    width="50"
-    color="blueviolet"
-    ariaLabel="three-circles-loading"
-    wrapperStyle={{}}
-    wrapperClass=""
-  />
+  <>
+    <div>Loading data...</div>
+    <ThreeCircles
+      visible={true}
+      height="30"
+      width="50"
+      color="blueviolet"
+      ariaLabel="three-circles-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+    />
+  </>
 );
 
 export default Loader;
